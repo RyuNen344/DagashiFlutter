e2e899e3174fc8e29ebec866a396425267fc2313
RyuNen344 <s1100633@outlook.com>
2021-01-10 06:56:37 +0900

    中華フォントが表示される問題の対応

--- lib/dagashi_app.dart
+++ lib/dagashi_app.dart
@@ -2,13 +2,19 @@ import 'package:dagashi_flutter/constants.dart';
 import 'package:dagashi_flutter/ui/mile_stone/mile_stone_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 
 class DagashiApp extends HookWidget {
+  static const Locale _locale = Locale('ja', 'JP');
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Dagashi',
+      locale: _locale,
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      supportedLocales: const [_locale],
       home: MileStonePage(),
       routes: {Constants.pageMileStone: (context) => MileStonePage()},
     );
